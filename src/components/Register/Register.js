00845e36d01691e50cfcab0ed0ff908688cd9d9e
Ishmum <ishmum20000@gmail.com>
2023-06-01 03:06:54 +0800

    Create register form

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import "./Register.css";
 
 const Register = () => {
+    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
-        <div>
-            
+        <div className="form-container">
+            <h2>Register</h2>
+            <Form>
+                <Row className="mb-3">
+                    <Col>
+                        <Form.Control
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="Name"
+                        />
+                    </Col>
+                    <Col>
+                        <Form.Control
+                            onChange={(e) => setUsername(e.target.value)}
+                            placeholder="Username"
+                        />
+                    </Col>
+                </Row>
+                <Form.Control
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="mb-3"
+                    type="email"
+                    placeholder="Email"
+                />
+                <Form.Control
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="mb-3"
+                    type="password"
+                    placeholder="Password"
+                />
+                <Button variant="dark" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
