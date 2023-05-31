ede556334a073277ebd828657fb73901eb2bcff6
Ishmum <ishmum20000@gmail.com>
2023-06-01 02:20:28 +0800

    Add Navbar and import bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Register from './components/Register/Register';
 import Home from './components/Home/Home';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navigation />
         <Routes>
           <Route exact path="/" Component={Home} />
           <Route exact path="/register" Component={Register}/>
