260f9afee295b99add47a67cc9a38a504a689601
Ishmum <ishmum20000@gmail.com>
2023-05-31 17:59:15 +0800

    Import components and react router dom elements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Router } from 'react-router';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/Register/Register';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
+        <Routes>
+          <Route exact path="/" Component={Home} />
+          <Route exact path="/register" Component={Register}/>
+        </Routes>
       </Router>
     </div>
   );
