ca52396127f38e2437d624eaa80b9c132a9a77e0
Ishmum <ishmum20000@gmail.com>
2023-06-07 21:29:10 +0800

    Create post api

--- server/index.js
+++ server/index.js
@@ -1,13 +1,50 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
-const cors = require("cors")
-const app = express();
-const port = 5000;
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//middleware
+app.use(express.json());
 app.use(cors());
+
+const uri =
+    "mongodb+srv://ishmum20000:password20000@to-do.zeo8n1v.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const usersCollection = client.db("to-do").collection("users");
+
+        //POST API
+        app.post("/register", async (req, res) => {
+            const user = req.body;
+
+            console.log("adding new user", req.body);
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        });
+    } finally {
+    }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(port, () => {
-    console.log("Listening at port: " + port);
+app.listen(PORT, () => {
+    console.log("Listening at port: " + PORT);
 });
